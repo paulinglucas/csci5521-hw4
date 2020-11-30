1f72feec8da61f6f36c8ac93f77a95a3b18554b4
Lucas Pauling <pauli086@umn.edu>
2020-11-29 18:21:45 -0600

    continued on pca

--- pca.py
+++ pca.py
@@ -1,9 +1,12 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def pca():
+    n = 600
+
     X = np.asarray(pd.read_csv("X_train.csv"))[:n]
-    Y = np.asarray(pd.read_csv("y_train.csv"))[:n].reshape([X_train.shape[0],])
+    Y = np.asarray(pd.read_csv("y_train.csv"))[:n].reshape([X.shape[0],])
 
     # Center data
     mean = np.mean(X,0)
@@ -15,6 +18,8 @@ def pca():
 
     # transform data with rotation
     X = X.dot(VT.T)
+
+    print(X[:,:2].shape)
 
 if __name__ == '__main__':
     pca()
