10451aa72ee08d6c8c08a8182fa898b9f08b5089
paren165 <paren165@umn.edu>
2020-12-01 18:31:19 -0600

    added fashion dataset to main

--- main.py
+++ main.py
@@ -18,12 +18,30 @@ def main():
     X_test = np.asarray(pd.read_csv("X_test.csv"))
     Y_test = np.asarray(pd.read_csv("y_test.csv")).reshape([X_test.shape[0],])
 
+    ## setup for fashion dataset
+    ftrain = np.genfromtxt("fashion-mnist_train.csv", delimiter=",")
+    ftrain0 = ftrain[ftrain[:, 0] == 0]
+    ftrain1 = ftrain[ftrain[:, 0] == 1]
+    X_ftrain = np.concatenate((ftrain0[:, 1:], ftrain1[:, 1:]))
+    Y_ftrain = np.concatenate((ftrain0[:, 0], ftrain1[:, 0]))
+    ftest = np.genfromtxt("fashion-mnist_test.csv", delimiter=",")
+    ftest0 = ftest[ftest[:, 0] == 0]
+    ftest1 = ftest[ftest[:, 0] == 1]
+    X_ftest = np.concatenate((ftest0[:, 1:], ftest1[:, 1:]))
+    Y_ftest = np.concatenate((ftest0[:, 0], ftest1[:, 0]))
+
+    X_ftrain /= 255
+    X_ftest /= 255
+
     ## #2
     data = [X_train, Y_train, X_test, Y_test]
+    fdata = [X_ftrain, Y_ftrain, X_ftest, Y_ftest]
     final_C_lin, error_lin = linearSVM(data)
+    ffinal_C_lin, ferror_lin = linearSVM(fdata)
 
     ## #3
     final_C_kern, error_kern = kernelSVM(data)
+    ffinal_C_kern, ferror_kern = kernelSVM(fdata)
 
     ## now compare the two for problem #4
     data_pca = pca()
