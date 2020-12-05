2dee59f786b7308a30d4f89126c41027af78bae1
Lucas Pauling <pauli086@umn.edu>
2020-12-05 12:42:49 -0600

    finished numbers 3 and 4

--- hw5/NN2pruned.py
+++ hw5/NN2pruned.py
@@ -11,27 +11,31 @@ X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1], [0.02,0.02, 1] , [0.98,0.02,1] ]
 y = np.array([[0,1,1,0,1,0]]).T                          # INPUT DATA LABELS
 
 syn0 = 2*np.random.random((3,4)) - 1                 # RANDOM INITIALIZATION
-syn1 = 2*np.random.random((4,1)) - 1
+syn1 = 2*np.random.random((4,3)) - 1                 # NEW HIDDEN LAYER
+syn2 = 2*np.random.random((3,1)) - 1
 diary=[]
 for j in range(60000):
    l1 = sigmoid(X.dot(syn0))                         # FEED FORWARD
    l2 = sigmoid(l1.dot(syn1))
-   l2_error = y - l2                                 # RECORD ERROR FOR TRACKING
-   diary.append(l2_error.T.dot(l2_error)[0][0])
+   l3 = sigmoid(l2.dot(syn2))
+   l3_error = y - l3                                 # RECORD ERROR FOR TRACKING
+   diary.append(l3_error.T.dot(l3_error)[0][0])
 
-   l2_delta = l2_error * dsigmoid(l2)                # BACK PROPAGATION
+   l3_delta = l3_error * dsigmoid(l3)                # BACK PROPAGATION
+   l2_delta = l3_delta.dot(syn2.T) * dsigmoid(l2)
    l1_delta = l2_delta.dot(syn1.T) * dsigmoid(l1)
+   syn2 += l2.T.dot(l3_delta)
    syn1 += l1.T.dot(l2_delta)
    syn0 += X.T.dot(l1_delta)
 
 print("final coefficients")                          # DISPLAY RESULTS
 print(str(syn0))
 print(str(syn1))
-print(sigmoid(sigmoid(X.dot(syn0)).dot(syn1)))
+print(str(syn2))
+print(sigmoid(sigmoid(sigmoid(X.dot(syn0)).dot(syn1)).dot(syn2)))
 
 def plot_error():
     plt.semilogy(diary)
     plt.show(block=False)
 
 print(" to see convergence, type: plot_error()")
-
--- hw5/problem3.py
+++ hw5/problem3.py
@@ -3,7 +3,7 @@ from sklearn.svm import SVC
 
 def main():
 
-    X = np.array([[0.2,0.8,1],[2,-1,1],[0.8,2,1],[2,1,1],[1.5,1,1],[-0.5,-0.5,1],[-0.5,-2,1],[-2,-1,1],[-1,-1.5,1]])
+    X = np.array([[0.2,0.8],[2,-1],[0.8,2],[2,1],[1.5,1],[-0.5,-0.5],[-0.5,-2],[-2,-1],[-1,-1.5]])
     y = np.array([1,1,1,1,1,-1,-1,-1,-1])
 
     clf = SVC(kernel='linear')
@@ -14,19 +14,56 @@ def main():
     print("a. Support vectors: \n{}\n".format(clf.support_vectors_))
 
     ## part b
-    print("b. Weight Vector: [{} {}]".format(round(clf.coef_[0][0],6),round(clf.coef_[0][1],6)))
-    print("Bias: {}".format(round(clf.intercept_[0],6)))
+    w = clf.coef_[0]
+    b = round(clf.intercept_[0],6)
+    print("b. Weight Vector: [{} {}]".format(round(w[0],6),round(w[1],6)))
+    print("Bias: {}".format(b))
+    print()
 
-    ## part c
+    ## part b
     print("c. w = c - d = ")
     alphas = abs(clf.dual_coef_)
     n = clf.n_support_[0]
-    print(alphas)
     c = np.sum((clf.support_vectors_[n:].T*alphas[0][n:]).T,0)
     d = np.sum((clf.support_vectors_[:n].T*alphas[0][:n]).T,0)
 
-    print("w = c - d = ")
+    print("w = c - d = ", end="")
     print(c, "-", d)
+    print()
+
+    ## part b
+    print("Decision Function: y = np.sign(w.T * X + b)")
+    print("= np.sign([{} {}].T * X + {})".format(round(w[0],6),round(w[1],6),round(b,6)))
+    x1 = X[0]
+    x2 = X[4]
+    x3 = X[7]
+
+
+    ## part c
+    print("c. Distance = (w.T*X + b) / norm(w)")
+    print("Distance from {}: {}".format(x1, abs(clf.decision_function([x1]) / np.linalg.norm(w))))
+    print("Distance from {}: {}".format(x2, abs(clf.decision_function([x2]) / np.linalg.norm(w))))
+    print("Distance from {}: {}".format(x3, abs(clf.decision_function([x3]) / np.linalg.norm(w))))
+    print()
+
+    ## part d
+    print("d. Removing (-0.5, -0.5) will change the decision boundary because", end="")
+    print("the point is a support vector. (0.8, 0.2) is not, so removing that point will", end="")
+    print("not affect the decision boundary")
+    print()
+
+    ## part e
+    print("e. This would lie on the positive decision boundary, and would certainly affect it.", end="")
+    print("We would proceed to then use RBF to fit the test data without error.")
+    print()
+
+    ## part f
+    print("f. Any value other than 1 will change the hard margin SVM to a soft margin SVM.", end="")
+    print("The bigger C is, the 'tighter' the decision boundary would become in order to fit", end="")
+    print("all of the training samples. The smaller C is, the 'looser' it would get, putting", end="")
+    print("less emphasis on a perfect fit, allowing 'stragglers' to be misclassified. Only support vectors", end="")
+    print("are affected.")
+    print()
 
 if __name__ == "__main__":
     main()
